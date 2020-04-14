a7c0a899d32e3f198346f146ca0eb1ca293e4bed
C3NZ <cenz@cenz.io>
2020-04-14 13:10:25 -0700

    [fix] the code so that it is magnitudes less complex and converts the dates into datetimes prior.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """
     Visualizing diets over the course of time
 """
+import datetime
 from functools import reduce
 
 import pandas as pd
@@ -11,6 +12,10 @@ app = Flask(__name__)
 # Attach our dataframe to our app
 app.loaded_csv = pd.read_csv("multiTimeline.csv", skiprows=1)
 app.loaded_csv.columns = ["month", "diet", "gym", "finance"]
+
+# Convert dates to datetimes
+app.loaded_csv["month"] = pd.to_datetime(
+    app.loaded_csv["month"], format="%Y-%d")
 
 
 @app.route("/", methods=["GET"])
@@ -27,29 +32,26 @@ def get_time_series_data():
     Return the necessary data to create a time series
     """
     # Grab the requested years and columns from the query arguments
-    range_of_years = [int(year) for year in request.args.getlist("n")]
-    trends = request.args.getlist("m")
+    range_of_years = [int(year) for year in request.args.getlist("years")]
+    trends = request.args.getlist("trends")
 
     # Generate a list of all the months we need to get
     min_year = min(range_of_years)
     max_year = max(range_of_years)
-    years_as_str = [str(year) for year in range(min_year, max_year + 1)]
 
-    # Combine the wanted months together.
-    wanted_months = reduce(
-        lambda a, b: a | b,
-        (app.loaded_csv["month"].str.contains(year) for year in years_as_str))
+    wanted_data = app.loaded_csv[
+        (app.loaded_csv["month"] >= datetime.datetime(min_year, 1, 1)) &
+        (app.loaded_csv["month"] <= datetime.datetime(max_year, 12, 31))
+    ]
 
-    # Create a new dataframe using the months that we've specified
-    sliced_df = app.loaded_csv[wanted_months][["month"] + trends]
-
-    # Convert all string dates into datetime objects and then sort them
-    sliced_df["month"] = pd.to_datetime(sliced_df["month"])
-    sliced_df = sliced_df.sort_values(by=["month"])
+    # Slice the DF to include only the trends we want and then to sort our
+    # dataframe by those trends.
+    wanted_data = wanted_data[["month"] + trends]
+    wanted_data = wanted_data.sort_values(by=["month"])
 
     # Return the dataframe as json
-    return sliced_df.to_json(), 200
+    return wanted_data.to_json(), 200
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
